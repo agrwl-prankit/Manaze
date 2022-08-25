54bd0bab7fc2cbbc425cc3c061b726fea27319ca
agrwl-prankit <agrwnlprankit77@gmail.com>
2022-08-26 02:12:50 +0530

    update UI in LoginActivity

--- app/src/main/java/com/prankit/manaze/activity/LoginActivity.java
+++ app/src/main/java/com/prankit/manaze/activity/LoginActivity.java
@@ -1,35 +1,71 @@
 package com.prankit.manaze.activity;
 
-import static java.security.AccessController.getContext;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.prankit.manaze.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputEditText inputEmail, inputPassword;
+    private TextInputLayout emailInputLayout, passwordInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        emailInputLayout = findViewById(R.id.inputEmailLayout);
+        passwordInputLayout = findViewById(R.id.inputPasswordLayout);
         inputEmail = findViewById(R.id.inputLoginEmail);
         inputPassword = findViewById(R.id.inputLoginPassword);
         TextView signIn = findViewById(R.id.signInTextView);
         TextView forgotPassword = findViewById(R.id.forgetPasswordText);
+
+        inputEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                emailInputLayout.setError(null);
+                passwordInputLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        inputPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                passwordInputLayout.setError(null);
+                emailInputLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
 
         forgotPassword.setOnClickListener(v -> Toast.makeText(LoginActivity.this, "Not Implemented yet", Toast.LENGTH_SHORT).show());
         signIn.setOnClickListener(v -> login(inputEmail.getText().toString(), inputPassword.getText().toString()));
@@ -37,10 +73,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private void login(String email, String password) {
 
-        Log.e("login button","button clicked");
-
-        if (!email.equals("")) inputEmail.setError("Required");
-        if (!password.equals("")) inputPassword.setError("Required");
+        if (email.equals("")) emailInputLayout.setError("Required");
+        if (password.equals("")) passwordInputLayout.setError("Required");
 
         if (!email.equals("") && !password.equals("")) {
             Toast.makeText(LoginActivity.this, "Logging In", Toast.LENGTH_SHORT).show();
@@ -48,5 +82,4 @@ public class LoginActivity extends AppCompatActivity {
             this.finish();
         }
     }
-
 }
